63f98d4616bec98579c7609086d26448639e9d44
devUserContact <developer.user.contact@protonmail.com>
2022-06-06 23:54:14 -0700

    linting

--- pages/GradientComponent/index.tsx
+++ pages/GradientComponent/index.tsx
@@ -1,44 +1,56 @@
-import type {NextPage} from 'next'
-import {useEffect, useCallback} from 'react'
+import type { NextPage } from "next";
+import { useCallback } from "react";
 
 const GradientComponent: NextPage = () => {
-
-	const randGen = (min, max) => {
+	const randGen = (min: number, max: number) => {
 		min = Math.ceil(min);
 		max = Math.floor(max);
-		const rand = Math.floor(Math.random() * (max - min + 1)) + min
-		return rand
-	}
+		const rand = Math.floor(Math.random() * (max - min + 1)) + min;
+		return rand;
+	};
 	const randomGradType = () => {
-		const gradType = new Array('linear', 'radial')
-		const randGradType = gradType[Math.floor(Math.random()*gradType.length)]
-		return randGradType
-	}
+		const gradType = new Array("linear", "radial");
+		const randGradType = gradType[Math.floor(Math.random() * gradType.length)];
+		return randGradType;
+	};
 	const randomColor = useCallback(() => {
-		const randRGB = (randGen(0, 255) + ',' + randGen(0, 255) + ',' + randGen(0, 255))
-		return randRGB
-	}, []) 	
+		const randRGB =
+			randGen(0, 255) + "," + randGen(0, 255) + "," + randGen(0, 255);
+		return randRGB;
+	}, []);
 	const randAngle = useCallback(() => {
-		const randAngle = (randGen(0, 90) + 'deg')
-		return randAngle
-	}, []) 	
+		const randAngle = randGen(0, 90) + "deg";
+		return randAngle;
+	}, []);
 	return (
 		<>
-			<div 
+			<div
 				style={{
-					height: '50%',
-					background: '' + randomGradType() + '-gradient(rgb(' + randomColor() + '), rgb(' + randomColor()  + '))'
+					height: "50%",
+					background:
+						"" +
+						randomGradType() +
+						"-gradient(rgb(" +
+						randomColor() +
+						"), rgb(" +
+						randomColor() +
+						"))",
 				}}
-			>
-  		</div> 
-			<div 
+			></div>
+			<div
 				style={{
-					height: '50%',
-					background: '' + randomGradType() + '-gradient(rgb(' + randomColor() + '), rgb(' + randomColor() + '))'
+					height: "50%",
+					background:
+						"" +
+						randomGradType() +
+						"-gradient(rgb(" +
+						randomColor() +
+						"), rgb(" +
+						randomColor() +
+						"))",
 				}}
-			>
-  		</div> 
-  	</>
-	)
-}
-export default GradientComponent
+			></div>
+		</>
+	);
+};
+export default GradientComponent;
